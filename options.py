a4bac30ffb4afbc6d78015651900ca4fe63193bb
Charlie Evans <evansc39@uni.coventry.ac.uk>
2019-03-16 22:05:19 +0000

    Adding python backend files

---
+++ options.py
@@ -0,0 +1,43 @@
+options = {
+
+	"a":"apple",
+	"b":"bucket",
+	"c":"cardigan",
+	"d":"dolphin",
+	"e":"elbow",
+	"f":"football",
+	"g":"grass",
+	"h":"hat",
+	"i":"ice",
+	"j":"joke",
+	"k":"key",
+	"l":"lemon",
+	"m":"mustache",
+	"n":"nigel farage",
+	"o":"onion",
+	"p":"paper",
+	"q":"queen",
+	"r":"rake",
+	"s":"sausage",
+	"t":"toad",
+	"u":"unicycle",
+	"v":"vomit",
+	"w":"water",
+	"x":"xenomorph",
+	"y":"yacht",
+	"z":"zinc"
+}
+
+scenarios = [
+
+	{"Scenario":"You are on the verge of starving. You have only one thing left to eat. Do you eat an apple, a key, grass?", "Options":["a","k","g"]},
+	{"Scenario":"You must lick one item. Do you choose your elbow, your cardigan or a lemon?", "Options":["e","c","l"]},
+	{"Scenario":"Which of these does the monopoly man currently not have in his possession: A mustache, vomit or a yacht?", "Options":["m","v","y"]},
+	{"Scenario":"You have the deciding vote. You may vote for Nigel Farage, a Dolphin, the Queen", "Options":["n","d","q"]},
+	{"Scenario":"Which of these most resembles a sphere: An Onion, a Football or a Hat?", "Options":["o","f","h"]},
+	{"Scenario":"An evil dragon stands in your way. What will you offer it: A Sausage, a Bucket or a Joke?", "Options":["s","b","j"]},
+	{"Scenario":"In a game of rock paper scissors, do you choose Toad, Rake, or Paper?", "Options":["t","r","p"]},
+	{"Scenario":"You are attacked in a dark alley. What do you defend yourself with: Ice, A Unicycle, A Xenomorph?", "Options":["i","u","k"]},
+	{"Scenario":"You wake up in the morning and the first thing you look for is water, zinc or sausage?", "Options":["w","z","s"]}
+	
+]
