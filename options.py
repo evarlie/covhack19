dbb4340d1f2e5b43ac5900e0742495d1f7262cbb
psel2000 <31243968+psel2000@users.noreply.github.com>
2019-03-17 02:01:00 +0000

    Update options.py
    This newer version of options.py contains retorts to users options

--- options.py
+++ options.py
@@ -30,14 +30,14 @@ options = {
 
 scenarios = [
 
-	{"Scenario":"You are on the verge of starving. You have only one thing left to eat. Do you eat an apple, a key, grass?", "Options":["a","k","g"]},
-	{"Scenario":"You must lick one item. Do you choose your elbow, your cardigan or a lemon?", "Options":["e","c","l"]},
-	{"Scenario":"Which of these does the monopoly man currently not have in his possession: A mustache, vomit or a yacht?", "Options":["m","v","y"]},
-	{"Scenario":"You have the deciding vote. You may vote for Nigel Farage, a Dolphin, the Queen", "Options":["n","d","q"]},
-	{"Scenario":"Which of these most resembles a sphere: An Onion, a Football or a Hat?", "Options":["o","f","h"]},
-	{"Scenario":"An evil dragon stands in your way. What will you offer it: A Sausage, a Bucket or a Joke?", "Options":["s","b","j"]},
-	{"Scenario":"In a game of rock paper scissors, do you choose Toad, Rake, or Paper?", "Options":["t","r","p"]},
-	{"Scenario":"You are attacked in a dark alley. What do you defend yourself with: Ice, A Unicycle, A Xenomorph?", "Options":["i","u","k"]},
-	{"Scenario":"You wake up in the morning and the first thing you look for is water, zinc or sausage?", "Options":["w","z","s"]}
+	{"Scenario":"You are on the verge of starving. You have only one thing left to eat. Do you eat an apple, a key, grass?", "Options":["a","k","g"], "a":"You take in one of your 5 a day", "k":"You can no longer get in your house", "g":"You graze"},
+	{"Scenario":"You must lick one item. Do you choose your elbow, your cardigan or a lemon?", "Options":["e","c","l"], "e":"Medical Marvel", "c":"It becomes unpleasant very quickly", "l":"It tastes very sour"},
+	{"Scenario":"Which of these does the monopoly man currently not have in his possession: A mustache, vomit or a yacht?", "Options":["m","v","y"], "m":"He feels insecure", "v":"He is currently healthy", "y":"He is upset"},
+	{"Scenario":"You have the deciding vote. You may vote for Nigel Farage, a Dolphin, the Queen", "Options":["n","d","q"], "n":"Brexit mean Brexit", "d":"Humans are put into aquariums", "q":"Democracy fails as she does not need to be elected"},
+	{"Scenario":"Which of these most resembles a sphere: An Onion, a Football or a Hat?", "Options":["o","f","h"], "o":"Close enough", "f":"Yes", "h":"???"},
+	{"Scenario":"An evil dragon stands in your way. What will you offer it: A Sausage, a Bucket or a Joke?", "Options":["s","b","j"], "s":"The dragon is content", "b":"The dragon is confused", "j":"The dragon attacks you"},
+	{"Scenario":"In a game of rock paper scissors, do you choose Toad, Rake, or Paper?", "Options":["t","r","p"], "t":"Your opponent walks away", "r":"Your opponent runs away", "p":"They chose scissors"},
+	{"Scenario":"You are attacked in a dark alley. What do you defend yourself with: Ice, A Unicycle, A Xenomorph?", "Options":["i","u","k"], "i":"The attacker starts to feel chilly", "u":"The attacker runs in fear", "k":"You successfully defend yourself but hurt countless others"},
+	{"Scenario":"You wake up in the morning and the first thing you look for is water, zinc or sausage?", "Options":["w","z","s"], "w":"It tastes nice", "z":"You feel better", "s":"It fills you up"}
 	
 ]
