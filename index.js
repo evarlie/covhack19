94a8120bbe239464a34ef63eb68e119793c39e3f
Caleb Parikh <parikhcaleb@gmail.com>
2019-03-17 08:40:03 +0000

    hacky http instant server solution for serving images

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ function loop(){
   console.log(key)
   // console.log(data.Options[Object.keys(data.Options)[0]])
 
-  document.getElementById("image").src = `images/${key}.png`
-// document.getElementById("image").src = `../images/${key}.png`
+  document.getElementById("image").src = `http://172.21.235.121:8080/${key}.png`
+ // document.getElementById("image").src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgiajp3QubOG7VD3CUkagxBEe3zwSXzqUscCmPZfGw4Bw4LItT'
 
 
   document.getElementById("train").addEventListener("click", () => {
@@ -99,7 +99,7 @@ function loop(){
 
     label++;
     var key = Object.keys(data.Options)[label];
-    document.getElementById("image").src = `images/${key}.png`;
+    document.getElementById("image").src = `http://172.21.235.121:8080/${key}.png`;
 
 
   })
@@ -153,6 +153,57 @@ function loop(){
 
   )
 }
+
+// background experimental
+
+// Some random colors
+// const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+const colors = ["#1B1B1B","#F4F1D6"];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11)/10,
+    y: Math.random() * 12/10
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+
+
+
+
+
+//
 
 
 
